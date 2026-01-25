ef809d5741a5b74a12aa8287732367dd72e7841a
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-01-25 17:05:37 +0600

    Added data type in JavaScript

---
+++ 02-data_type.js
@@ -0,0 +1,27 @@
+/**
+ * Two (2) types of data type in JavaScript
+ * 1. Primitive data type
+ *     i. string, ii. number, iii. boolean, iv. undefined, v. null
+ * 2. Non primitive or referencial data type
+ *    i. array , ii. object
+ */
+
+// Primitive ***
+
+// i. string
+let birthPlace = "Kurigram, Rangpur, Bangladesh";
+
+// ii. number
+let age = 25; //Integer অথবা পূর্ণসংখ্যা
+let cgpa = 4.64; //floating number অথবা দশমিক সংখ্যা
+
+// iii. boolean: true or false
+let isMarried = false;
+
+// iv. undefined [অর্থাৎ ভেরিয়েবল ডিকলেয়ার করা হয়েছে কিন্তু কোন ভ্যালু বা কিছু ডিফাইন করা হয়নি।]
+let schoolName; 
+console.log(schoolName);
+
+// v. null [অর্থাৎ ভেরিয়েবল ডিকলেয়ার করা হয়েছে এবং ভ্যালু ডিফাইন করা হয়েছে কিন্তু ভ্যালুতে কোন মান দেওয়া হয়নি, শুধু null দেওয়া হয়েছে।]
+let collegeName = null;
+console.log(collegeName);
