55b3bbdd3d5f4ccca0db0ee5b066de4ddf436921
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-04 16:25:57 +0600

    Problem solving

---
+++ 11.1-problem_solving_find-out-get-strawberry.js
@@ -0,0 +1,30 @@
+// প্রশ্ন: দুইজনের মধ্যে যে বেশি নাম্বার পাবে বা যার নাম্বার বেশি হবে সে strawberry উইনার হবে।
+
+
+// Way 01
+
+const kabul = 65;
+const cabul = 70;
+
+if(kabul > cabul){
+    console.log("kabul will get the strawberry");
+}
+
+else{
+    console.log("cabul will get the strawberry");
+}
+
+// Way 02
+
+function willGet (num1, num2){
+    if (num1 > num2){
+        return num1
+    }
+
+    else{
+        return num2
+    }
+}
+
+const finalGet = willGet(30, 20);
+console.log("Congratulations, you are winner", finalGet);
