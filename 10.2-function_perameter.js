4de592adbc5035187b3041de251e5ba3d93e9022
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-04 16:18:12 +0600

    Function

---
+++ 10.2-function_perameter.js
@@ -0,0 +1,42 @@
+// বর্গ প্যারামিটার
+
+function squar (number){
+    const borgo = number * number;
+    console.log(borgo);    
+}
+
+squar (505);
+
+// যোগ প্যারামিটার
+
+function add (a, b){
+    const sum = a + b;
+    console.log(sum);
+}
+
+add(1,82);
+
+function addMultiple (x, y, z){
+    const multipleAdd = x+y+z;
+    console.log(multipleAdd)
+}
+
+addMultiple(25, 65, 80);
+
+// বিয়োগ প্যারামিটার
+
+function Subtraction (z, x){
+    const biyog = z-x;
+    console.log(biyog);
+}
+
+Subtraction(875, 874);
+
+//ভাগ প্যারামিটার
+
+function division (x, y){
+    const vag = x/y;
+    console.log(vag);
+}
+
+division(100, 10);
