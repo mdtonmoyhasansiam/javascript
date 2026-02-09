a5e038bc1a1332224983fcb51eb5b40561210e61
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-09 17:26:55 +0600

    Bonus

---
+++ 13-bonus.js
@@ -0,0 +1,25 @@
+
+const multipleObj = {
+    Name: "Jaxon",
+    DOB: 2000,
+    Address: "LA, USA",
+    isMarried: false,
+    carCollection: [
+        {Name: "BMW", CC: 1500, Color: "Yellow"},
+        {Name: "BYD",  CC: 1600, Color: "Black"},
+    ],
+    CarVarient: ["USA", "UAE"],
+    CarOwner: function(){
+        console.log("Own here Jaxon");
+    },
+    Acting: {
+        Name: "BMW",
+        Relese: 2005,
+    }
+
+
+
+    
+}
+
+console.log(multipleObj);
