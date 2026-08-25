c68ce210765970841fc181ebc49a2cce777d8d3d
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-08-25 22:16:19 +0600

    Variables and data types practice

---
+++ a-variable.js
@@ -0,0 +1,23 @@
+// console.log("Adib");
+
+
+let age = 3;
+age = 5;
+age = 10;
+console.log(age);
+
+
+const name = "Adib";
+console.log(name);
+
+const dathOfBirth = "10/10/2000";
+console.log(dathOfBirth);
+
+let isAdult = false;
+console.log(isAdult);
+
+console.log(typeof (age), typeof(name), typeof(dathOfBirth), typeof(isAdult));
+
+
+// ***JavaScript Data Types
+// ssbbnnuo => String, Symbol, Big Int, Boolean, Number, Null, Undefined, Object*** 
