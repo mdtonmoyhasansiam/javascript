bb37f94e6241f431aaa06a89f5ac13eeab415ca5
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-08-26 14:24:19 +0600

    JS operators

---
+++ b-operators.js
@@ -0,0 +1,27 @@
+// Arithmetic operators: + - * / %
+// Assignment operators: = =+ -= *= /=
+// Comparison operators: == === < > =< >= 
+// Logical operators: && || !
+
+
+let a = 10
+let b = 15
+
+let sum = a + b
+// console.log(sum);
+
+let division = a / b
+// console.log(division);
+
+let modulas = a % b
+// console.log(modulas);
+
+
+
+
+let sum1 = sum + 10
+console.log(sum1);
+
+
+let sum2 = !(a > b) && 10
+console.log(sum2);
