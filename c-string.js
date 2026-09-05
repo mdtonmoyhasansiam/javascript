2941ab69295cec904ccb10b4fa093ec2a945e517
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-09-05 11:51:35 +0600

    Practice JS literals

--- c-string.js
+++ c-string.js
@@ -2,5 +2,6 @@
 let firstName = "Michel"
 let lastName = "Jaxon"
 
-let fullName = firstName + " " + lastName;
+// let fullName = firstName + " " + lastName;
+let fullName = `${firstName} ${lastName} \nGazipur`
 console.log(fullName);
