ca7782851fb02148125832539dd52fd6d0e91b75
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-09-05 11:59:23 +0600

    Practice JS method

--- c-string.js
+++ c-string.js
@@ -4,4 +4,11 @@ let lastName = "Jaxon"
 
 // let fullName = firstName + " " + lastName;
 let fullName = `${firstName} ${lastName} \nGazipur`
-console.log(fullName);
+// console.log(fullName);
+
+let randomstr = "I'm a \"student\""
+// console.log(randomstr);
+
+console.log(randomstr.toUpperCase());
+console.log(randomstr.includes("student"));
+console.log(randomstr.length);
