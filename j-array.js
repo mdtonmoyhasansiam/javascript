11029fe65eb2b3b4396da0197cbffb318259ee33
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-09-20 12:28:11 +0600

    Array method

---
+++ j-array.js
@@ -0,0 +1,21 @@
+// array
+
+let name = ["Mofiz", "Kuddus", "Josim", "Chuddus"]
+let number = [100, 200, 300, 500]
+
+// name.push("Luddus");
+// name.pop();
+
+// name.unshift("Luddus");
+// name.shift();
+
+// name.splice(1, 1, "Luddus");
+
+let result = name.slice(1, 3);
+let result1 = name.slice(-3, -1);
+console.log(result, result1);
+
+
+// console.log(name);
+
+// console.log(name[2], number[2]);
