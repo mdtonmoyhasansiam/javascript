741972c7451e39619e79d26360874f4e2825f968
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-09-20 17:23:28 +0600

    for of and for in loop

--- l-loop.js
+++ l-loop.js
@@ -18,9 +18,27 @@ let food = ["Chiken Fry", "Frens Fry", "Fried Rice"]
 //     i++;
 // }
 
-let i = 0;
-while(i<food.length){
-    console.log(`${food[i]} khabooooooo`);
-    i++;
+// let i = 0;
+// while(i<food.length){
+//     console.log(`${food[i]} khabooooooo`);
+//     i++;
+// }
+
+
+// for of loop
+// for (let foodie of food){
+//     console.log(foodie, "Khabooo");
+// }
+
+
+// for in loop
+
+let me = {
+    name: "Siam",
+    age: 10,
+    address: "Dhaka"
 }
 
+for (let my in me){
+    console.log(my, "->", me[my]);
+}
