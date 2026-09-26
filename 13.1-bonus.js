92136e0cb582f88b158848fc25cb105fafd145fa
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-09-26 16:26:18 +0600

    ....

--- 13.1-bonus.js
+++ 13.1-bonus.js
@@ -8,3 +8,14 @@ function justTest(num1, num2){
 
 const result = justTest(12, 25, 10, 58, 89);
 console.log(result);
+
+// Duplicate
+
+function justTest(num1, num2){
+    console.log(num1, num2);
+    console.log(arguments);
+    console.log(arguments[3]);
+}
+
+const result = justTest(12, 25, 10, 58, 89);
+console.log(result);
